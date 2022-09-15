96490fb3566706d3b8b6504fd1a749b0ff6e12f3
Tomáš Jelínek <73285347+Desperadus@users.noreply.github.com>
2022-09-15 18:28:44 +0200

    Rcpp fixes
    still problem with calling build_boundary_Cu, but this can be fixed by calling devtools::load_all(".") after library(HodgePaths)

--- R/HodgeDecomp.R
+++ R/HodgeDecomp.R
@@ -2,22 +2,60 @@
 #'
 #' @param BB List of boundary matricies (return of complex_to_boundaryF(...))
 #' @param derahm DeRahm map on 1-simplicies
+#' @param alg Algorithm for lsqr possibilities: "pinv", "minres" and "bicg"
 #'
-#' @return Returns a list where 1st entry is gradient field, 2nd is curl field and 3rd is harmonic component
+#' @return Returns a list where 1st entry is gradient field, 2nd is curl field and 3rd is harmonic the component
 #' @export
 #'
 #' @examples
-HodgeDecomp <- function(BB,derahm) {
+
+HodgeDecomp <- function(BB,derahm,alg="minres") {
   dif=Matrix::t(BB[[1]])
   codif=Matrix::t(BB[[2]])
   adjdif=BB[[1]]
   adjcodif=BB[[2]]
-  A=pracma::pinv(as.matrix(adjdif%*%dif))
-  s=A%*%(adjdif%*%derahm)
-  yg=dif%*%s
-  A2=pracma::pinv(as.matrix(codif%*%adjcodif))
-  z=A2%*%(codif%*%derahm)
-  yc=adjcodif%*%z
-  yh=derahm-yg-yc
+
+  A1<-adjdif%*%dif
+  b1<-adjdif%*%derahm
+
+  A2<-codif%*%adjcodif
+  b2<-codif%*%derahm
+
+
+  if (alg=="minres") {
+    res1<-minres(A1,as.numeric(b1))
+    yg<-dif%*%res1$x
+
+    res2<-minres(A2,as.numeric(b2))
+    yc<-adjcodif%*%res2$x
+
+    yh<-derahm-yg-yc
+  }
+
+  else if (alg=="pinv"){
+    s<-pracma::pinv(as.matrix(A1))%*%(b1)
+    yg<-dif%*%s
+    z<-pracma::pinv(as.matrix(A2))%*%(b2)
+    yc<-adjcodif%*%z
+    yh<-derahm-yg-yc
+  }
+
+  else if (alg=="bicg") {
+    res1<-bicgSparse(A1,as.numeric(b1))
+    yg<-dif%*%res1$x
+
+    res2<-bicgSparse(A2,as.numeric(b2))
+    yc<-adjcodif%*%res2$x
+
+    yh<-derahm-yg-yc
+  }
+  else {
+    stop("Incorrect algorithm for calculation of lsqr entered")
+  }
+
+
+
   return(list(yg,yc,yh))
 }
+
+
---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' HodgePaths
+#'
+#' Does Hodge decomposition of vector field associated with point cloud
+#'
+#' @docType package
+#' @author Jan Stuchly
+#' @import Rcpp RcppEigen
+#' @importFrom Rcpp evalCpp
+#' @useDynLib HodgePaths
+#' @name HodgePaths
+NULL
